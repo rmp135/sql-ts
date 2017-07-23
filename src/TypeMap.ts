3c3104ca29668c17d6eaea4837520a7f9cc8bdc3
Ryan Poole <rmp135@gmail.com>
2017-07-23 15:53:33 +0100

    Added additional types to the TypeMap.
    Due to a change in how the default type is no longer string.

--- src/TypeMap.ts
+++ src/TypeMap.ts
@@ -1,4 +1,5 @@
 export default {
+  string: ['nchar', 'nvarchar', 'varchar', 'char', 'tinytext', 'text', 'longtext', 'mediumtext', 'ntext', 'varbinary', 'uuid'],
   number: ['int', 'numeric', 'integer', 'real', 'smallint', 'decimal', 'float', 'double precision', 'double', 'dec', 'fixed', 'year', 'serial', 'bigserial'],
   Date: ['datetime', 'timestamp', 'date', 'time', 'timestamp'],
   boolean: ['bit', 'boolean', 'bool'],
