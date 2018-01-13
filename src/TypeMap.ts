e788a0e24202900e2c534acebef827becc2364c1
Ryan Poole <rmp135@gmail.com>
2018-01-13 16:33:48 +0000

    Additional mssql types.

--- src/TypeMap.ts
+++ src/TypeMap.ts
@@ -1,7 +1,7 @@
 export default {
   string: ['nchar', 'nvarchar', 'varchar', 'char', 'tinytext', 'text', 'longtext', 'mediumtext', 'ntext', 'varbinary', 'uuid'],
-  number: ['int', 'numeric', 'integer', 'real', 'smallint', 'decimal', 'float', 'double precision', 'double', 'dec', 'fixed', 'year', 'serial', 'bigserial'],
-  Date: ['datetime', 'timestamp', 'date', 'time', 'timestamp'],
+  number: ['int', 'numeric', 'integer', 'real', 'smallint', 'decimal', 'float', 'double precision', 'double', 'dec', 'fixed', 'year', 'serial', 'bigserial', 'bigint'],
+  Date: ['datetime', 'timestamp', 'date', 'time', 'timestamp', 'datetime2', 'smalldatetime'],
   boolean: ['bit', 'boolean', 'bool'],
   Object: ['json']
 }
