588b8f965fa96aec99e54c9eb480e0aa601f6b3f
Ryan Poole <rmp135@gmail.com>
2018-02-07 21:35:35 +0000

    0.3.3

--- dist/TypeMap.js
+++ dist/TypeMap.js
@@ -2,8 +2,8 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = {
     string: ['nchar', 'nvarchar', 'varchar', 'char', 'tinytext', 'text', 'longtext', 'mediumtext', 'ntext', 'varbinary', 'uuid'],
-    number: ['int', 'numeric', 'integer', 'real', 'smallint', 'decimal', 'float', 'double precision', 'double', 'dec', 'fixed', 'year', 'serial', 'bigserial'],
-    Date: ['datetime', 'timestamp', 'date', 'time', 'timestamp'],
+    number: ['int', 'numeric', 'integer', 'real', 'smallint', 'decimal', 'float', 'double precision', 'double', 'dec', 'fixed', 'year', 'serial', 'bigserial', 'bigint'],
+    Date: ['datetime', 'timestamp', 'date', 'time', 'timestamp', 'datetime2', 'smalldatetime'],
     boolean: ['bit', 'boolean', 'bool'],
     Object: ['json']
 };
