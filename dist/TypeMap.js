8253a2995e58cd819d4e6a5d996e5926ca4149de
Saeid Ostad <SOstad@kajimausa.com>
2020-01-13 13:26:25 -0500

    added missing sql server types in TypeMap

--- dist/TypeMap.js
+++ dist/TypeMap.js
@@ -1,8 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = {
-    string: ['nchar', 'nvarchar', 'varchar', 'char', 'tinytext', 'text', 'longtext', 'mediumtext', 'ntext', 'varbinary', 'uuid', 'character varying'],
-    number: ['int', 'numeric', 'integer', 'real', 'smallint', 'decimal', 'float', 'double precision', 'double', 'dec', 'fixed', 'year', 'serial', 'bigserial', 'bigint', 'int4'],
+    string: ['nchar', 'nvarchar', 'varchar', 'char', 'tinytext', 'text', 'longtext', 'mediumtext', 'ntext', 'varbinary', 'uuid', 'uniqueidentifier', 'character varying'],
+    number: ['tinyint', 'int', 'numeric', 'integer', 'real', 'smallint', 'decimal', 'float', 'double precision', 'double', 'dec', 'fixed', 'year', 'serial', 'bigserial', 'bigint', 'int4'],
     Date: ['datetime', 'timestamp', 'date', 'time', 'timestamp', 'datetime2', 'smalldatetime'],
     boolean: ['bit', 'boolean', 'bool'],
     Object: ['json']
