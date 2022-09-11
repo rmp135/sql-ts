264785d90b5b4b6cd13b4b218bd82d3163e26eca
Laurent Cozic <laurent@cozic.net>
2022-09-11 09:53:43 +0100

    Use the raw value as default value

--- src/Adapters/sqlite.ts
+++ src/Adapters/sqlite.ts
@@ -3,50 +3,6 @@ import { AdapterInterface, TableDefinition, ColumnDefinition, EnumDefinition } f
 import { Config } from '..'
 import * as SharedAdapterTasks from './SharedAdapterTasks'
 
-// SQLite default values are always strings regardless of the column types. So
-// here we convert it to the correct type.
-const formatDefaultValue = (value:string | null, type: string):null | number | string => {
-  if (value === null) return value;
-
-  // From https://www.sqlite.org/datatype3.html
-  //
-  // Note that DECIMAL(10,5) is also a valid type - this is checked in the
-  // conditional below
-  const numericTypes = [
-    'INT',
-    'INTEGER',
-    'TINYINT',
-    'SMALLINT',
-    'MEDIUMINT',
-    'BIGINT',
-    'UNSIGNED BIG INT',
-    'INT2',
-    'INT8',
-    'REAL',
-    'DOUBLE',
-    'DOUBLE PRECISION',
-    'FLOAT',
-    'NUMERIC',
-    'BOOLEAN',
-    'DATE',
-    'DATETIME',
-  ];
-
-  // SQLite default values are always surrounded by quotes - eg `"3"` (for
-  // numeric value 3) or `"example"` (for string `example`). So here we remove
-  // the quotes, but to safe we check that they are actually present.
-  if (value.length && value[0] === '"') value = value.substr(1);
-  if (value.length && value[value.length - 1] === '"') value = value.substr(0, value.length - 1);
-
-  type = type.toUpperCase();
-
-  if (numericTypes.includes(type) || type.startsWith('DECIMAL')) {
-    return Number(value);
-  } else {
-    return value;
-  }
-}
-
 export default class implements AdapterInterface {
   async getAllEnums(db: Knex, config: Config): Promise<EnumDefinition[]> {
     return await SharedAdapterTasks.getTableEnums(db, config)
@@ -71,7 +27,7 @@ export default class implements AdapterInterface {
         isEnum: false,
         isPrimaryKey: c.pk !== 0,
         comment: '',
-        defaultValue: formatDefaultValue(c.dflt_value, c.type),
+        defaultValue: c.dflt_value,
       } as ColumnDefinition
     ))
   }
